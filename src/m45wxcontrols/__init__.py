1e67d98b36f4b2e3ba2167bbaa70e45c752e86d2
Steffen Schultz <steffenschultz@mailbox.org>
2026-06-15 15:54:24 +0200

    Create pyproject.toml and update init

--- src/m45wxcontrols/__init__.py
+++ src/m45wxcontrols/__init__.py
@@ -1,3 +1,12 @@
 """
 Special controls for wxPython by M45Development
 """
+from .accessible_spin import AccessibleSpinCtrl
+from .custom_text_entry import CustomTextEntryDialog
+from .universal_list import UniversalListCtrl
+
+__all__ = [
+    "AccessibleSpinCtrl",
+    "CustomTextEntryDialog",
+    "UniversalListCtrl",
+]
